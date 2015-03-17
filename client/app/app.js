d8666a15e6613ee7ae8420c8d251fae635dad0e2
iksose <iksose.pt3@gmail.com>
2015-03-17 11:35:39 -0500

    first status check

--- client/app/app.js
+++ client/app/app.js
@@ -1,24 +1,5 @@
-// Make sure to include the `ui.router` module as a dependency
-var app = angular.module('simple_monitor', [
-    'ui.router'
-  ])
-  .config(
-    ['$stateProvider', '$urlRouterProvider', '$locationProvider',
-      function($stateProvider, $urlRouterProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        // If the url is ever invalid, e.g. '/asdf', then redirect to '/' aka the home state
-        $urlRouterProvider.otherwise('/');
-
-        $stateProvider
-          .state('root', {
-            url: '/',
-            views: {
-              'container': {
-                templateUrl: 'client/app/home/home.html',
-                controller: 'homeCtrl as homeCtrl'
-              }
-            }
-          })
-      }
-    ]
-  );
+$.ajax({
+  url: "/api/secrets",
+}).done(function(data) {
+  document.getElementsByClassName("serverstatus")[0].innerHTML += " 200"
+});
